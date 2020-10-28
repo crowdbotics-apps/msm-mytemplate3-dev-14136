c858382781bda8ebfe7b6d24be87e3c179cbc29f
Crowdbotics <team@crowdbotics.com>
2020-10-28 08:26:05 +0000

    add package blueprint-react-native-emailauth-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,5 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
+{ name: 'EmailAuth313339', human_name: 'Email Auth 3', access_route: 'EmailAuth313339', icon: 'envelope-o'},
 
   // you can add more installed blueprints here
   // access route is the route nate given to navigator
--- src/redux/mainSaga.js
+++ src/redux/mainSaga.js
@@ -2,6 +2,7 @@ import { all, takeEvery, take } from "redux-saga/effects";
 
 
 //@BlueprintReduxSagaImportInsertion
+import EmailAuth313339Saga from '../features/EmailAuth313339/redux/sagas';
 
 function* helloSaga() {
   console.log("Hello from saga!");
@@ -14,6 +15,7 @@ export function* mainSaga() {
 
 
     //@BlueprintReduxSagaMainInsertion
+EmailAuth313339Saga,
     
   ]);
 }
